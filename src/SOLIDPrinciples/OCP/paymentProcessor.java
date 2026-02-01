33d7787cf47c8e130f0afebc5b645d47b78f3055
Rachit Misra <rachitmisra31@gmail.com>
2026-02-01 21:42:53 +0530

    Payment Processor class

--- src/SOLIDPrinciples/OCP/paymentProcessor.java
+++ src/SOLIDPrinciples/OCP/paymentProcessor.java
@@ -1,5 +1,23 @@
 package SOLIDPrinciples.OCP;
 
+/** Violates OCP Principle:
+ * public class PaymentProcessorBad {
+ *     public void processPayment(String paymentMethod,double amount){
+ *         if(paymentMethod.equals("CreditCard")){
+ *             System.out.println("Credit Card Payment done" + amount);
+ *         }
+ *         else if(paymentMethod.equals("DebitCard")){
+ *             System.out.println("Debit Card Payment done" + amount);
+ *         }
+ *         else if(paymentMethod.equals("Fampay")){
+ *             System.out.println("Fampay Payment done" + amount);
+ *         }
+ *         else throw new IllegalArgumentException("Invalid payment method");
+ *     }
+ * }
+ */
 public class paymentProcessor {
-    public void paymentProcessor(paymentMethod )
+    public void paymentProcessor(paymentMethod paymentMethod,double amount) {
+        paymentMethod.pay(amount);
+    }
 }
