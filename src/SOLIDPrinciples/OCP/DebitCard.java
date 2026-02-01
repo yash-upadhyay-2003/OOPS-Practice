7e836e219a86e08acdff20359e036f38ff9e3e6a
Rachit Misra <rachitmisra31@gmail.com>
2026-02-01 21:41:01 +0530

    Debit card class commit

--- src/SOLIDPrinciples/OCP/DebitCard.java
+++ src/SOLIDPrinciples/OCP/DebitCard.java
@@ -1,7 +1,8 @@
 package SOLIDPrinciples.OCP;
 
-public class DebitCard implements paymentMethod{
+public class DebitCard implements paymentMethod {
+    @Override
     public void pay(double amount) {
-        System.out.println("Payment is done using credit card");
+        System.out.println("Payment is done using Debit card " + amount);
     }
 }
